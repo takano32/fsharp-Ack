7c97f2e589d2395ed763ba16855f19e92af0900c
TAKANO Mitsuhiro <takano32@gmail.com>
2012-09-14 18:11:22 +0900

    fix

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ ack.exe:	ack.fsx
 	fsharpc2 $<
 
 run:	ack.exe
-	mono $<
+	time mono $<
 
 clean:
 	rm -f ack.exe
--- ack.fsx
+++ ack.fsx
@@ -5,14 +5,17 @@ let rec ack (m, n) =
   | (_, 0) -> ack (m-1, 1)
   | (_, _) -> ack (m-1, ack (m, n-1)) ;;
 
-
+(*
 printfn "A(1, 1) = %d" (ack (1, 1)) ;;
 printfn "A(2, 2) = %d" (ack (2, 2)) ;;
 printfn "A(4, 0) = %d" (ack (4, 0)) ;;
 printfn "A(3, 1) = %d" (ack (3, 1)) ;;
 printfn "A(3, 4) = %d" (ack (3, 4)) ;;
+*)
+
 printfn "A(4, 1) = %d" (ack (4, 1)) ;;
 
+(*
 printfn "A(4, 2) = %d" (ack (4, 2)) ;;
 
 printfn "A(4, 3) = %d" (ack (4, 3)) ;;
@@ -20,4 +23,4 @@ printfn "A(4, 3) = %d" (ack (4, 3)) ;;
 printfn "A(4, 4) = %d" (ack (4, 4)) ;;
 
 printfn "A(5, 2) = %d" (ack (5, 2)) ;;
-
+*)
